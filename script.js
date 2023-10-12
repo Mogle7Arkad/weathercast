6ff8131ef936ba4a1619346ce58f0efa63a5e65a
Ali George <george7ali@gmail.com>
2023-10-12 15:43:02 +0100

    implemented the search input and the button to enable search for a city

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+const searchInput = document.querySelector('#search input');
+const searchBtn = document.querySelector('#search button');
 const temperature = document.querySelector('#temp');
-const city = document.querySelector('#city');
+const searchCity = document.querySelector('#city');
 const humidity = document.querySelector('#humidity');
 const windSpeed = document.querySelector('#windSpeed');
 
 const apiKey = '647a6068d1d73ad2a9ee14c8ceb0b28c';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=kafanchan';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     let data = await response.json();
 
     temperature.innerHTML = Math.round(data.main.temp) + ' °C';
-    city.innerHTML = data.name;
+    searchCity.innerHTML = data.name;
     humidity.innerHTML = data.main.humidity + ' %';
     windSpeed.innerHTML = data.wind.speed + ' km/h';
 }
-checkWeather();
+
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    checkWeather(searchInput.value);
+})
